4a73f48bfdab61de6f978c3178777e2830037d79
T1mbits <timbits1123@gmail.com>
2024-03-01 23:23:23 -0500

    config file generation and verification

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,42 +1,82 @@
 pub mod structure;
 
+use dirs::config_dir;
 use std::{fs, path::Path};
 use toml::{from_str, to_string};
 
 use structure::DConfig;
 
-const CONFIG_PATH: &str = "../../drpc.toml";
+/// Generates the directory for where the config file should be located.
+///
+/// Use this function in case of different OS or no accessible configuration directory environment variable
+fn generate_config_dir_path() -> String {
+    match config_dir() {
+        Some(config_dir) => match config_dir.to_str() {
+            None => "./drpc".to_owned(),
+            Some(config_dir) => config_dir.to_owned() + "/drpc",
+        },
+        None => "./drpc".to_owned(),
+    }
+}
+
+/// Generates the file location for where the config file should be located.
+///
+/// Uses `generate_config_dir_path()` and appends file location.
+fn generate_config_file_path() -> String {
+    generate_config_dir_path() + "/drpc.toml"
+}
 
 pub fn initialize_config() -> DConfig {
-    if Path::new(CONFIG_PATH).exists() {
+    if Path::new(&generate_config_file_path()).exists() {
         return read_config_file();
     };
     DConfig::default()
 }
 
 pub fn write_config(config: &DConfig) -> () {
+    let config_dir: String = generate_config_dir_path();
+    let config_file: String = generate_config_file_path();
+
     let serialized_config: String = match to_string(config) {
         Ok(serialized_config) => serialized_config,
         Err(error) => panic!("Error while serializing config data: {}", error),
     };
-    match fs::write(CONFIG_PATH, serialized_config) {
-        Ok(_) => {}
+
+    if !Path::new(&config_dir).exists() {
+        match fs::create_dir_all(&config_dir) {
+            Err(error) => panic!("Error while creating config directory: {}", error),
+            Ok(_) => println!("Created directory {}", config_dir),
+        }
+    }
+
+    match fs::write(&config_file, serialized_config) {
+        Ok(_) => println!("Wrote to file {}", config_file),
         Err(error) => panic!("Error while writing config: {}", error),
     }
 }
 
 pub fn read_config_file() -> DConfig {
-    match fs::read(CONFIG_PATH) {
-        Ok(config) => {
-            match from_str(
-                String::from_utf8(config)
-                    .expect("umm what did you do to my config?")
-                    .as_str(),
-            ) {
-                Ok(config) => config,
-                Err(error) => panic!("Error while deserializing config: {}", error),
+    let config_file: String = generate_config_file_path();
+    match fs::read(&config_file) {
+        Ok(config_vector) => verify_config_integrity(config_vector, config_file),
+        Err(error) => panic!("Error while reading config at {}: {}", config_file, error),
+    }
+}
+
+fn verify_config_integrity(config_vector: Vec<u8>, config_file: String) -> DConfig {
+    let config_string: String = match String::from_utf8(config_vector) {
+        Err(_) => panic!("There's no way that's a valid config file"),
+        Ok(decoded_string) => decoded_string,
+    };
+    match from_str(&config_string) {
+        Err(error) => {
+            println!("Error while deserializing configuration file: {}", error);
+            match fs::remove_file(config_file) {
+                Ok(_) => println!("Removed invalid configuration file"),
+                Err(error) => panic!("Error while removing invalid configuration file: {}", error),
             }
+            initialize_config()
         }
-        Err(error) => panic!("Error while reading config at {}: {}", CONFIG_PATH, error),
+        Ok(config) => config,
     }
 }
