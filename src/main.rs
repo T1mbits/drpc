f5a95042902f39ff4b867c87184f6a4206f2c271
T1mbits <timbits1123@gmail.com>
2024-03-02 16:22:04 -0500

    simplified main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,28 @@
 pub mod config;
+pub mod daemon;
 pub mod parser;
 
-use discord_rpc_client::Client;
+// use discord_rpc_client::Client;
 
-use config::{initialize_config, structure::DConfig, write_config};
+// use config::{initialize_config, structure::DConfig, write_config};
 use parser::parse_command;
 
 fn main() {
-    let config: DConfig = initialize_config();
-    let mut client: Client = Client::new(config.discord.client_id);
-    client.start();
-    client.on_ready(|_context| {
-        println!("Discord Client ready!");
-    });
-    client.on_error(|context| println!("Error: {:?}", context));
-    match client.set_activity(|activity| activity.state("test")) {
-        Ok(_) => {}
-        Err(error) => {
-            println!("Error while setting activity: {}", error)
-        }
-    }
-    ();
-    println!("should've set activity now");
-    write_config(&config);
+    // let config: DConfig = initialize_config();
+    // let mut client: Client = Client::new(config.discord.client_id);
+    // client.start();
+    // client.on_ready(|_context| {
+    //     println!("Discord Client ready!");
+    // });
+    // client.on_error(|context| println!("Error: {:?}", context));
+    // match client.set_activity(|activity| activity.state("test")) {
+    //     Ok(_) => {}
+    //     Err(error) => {
+    //         println!("Error while setting activity: {}", error)
+    //     }
+    // }
+    // ();
+    // println!("should've set activity now");
+    // write_config(&config);
     parse_command();
-    loop {}
 }
