15270d7e4105c9d0718624ba0a2b2848220d4684
T1mbits <timbits1123@gmail.com>
2024-03-06 13:25:07 -0500

    scuffed logging system

---
+++ src/logging.rs
@@ -0,0 +1,50 @@
+use chrono::Local;
+use std::io::prelude::*;
+
+pub fn ddrpc_log(data: &str) {
+    let mut file = std::fs::OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open("/home/Timbits/.config/ddrpc/daemon-log.txt")
+        .unwrap();
+    let time = Local::now();
+
+    let mut data = data.to_owned();
+    data.retain(|char| char != '\n');
+
+    if let Err(e) = writeln!(file, "{time} > {data}") {
+        panic!("Error: {}", e);
+    }
+}
+
+fn daemon_log(data: String) {
+    let mut file = std::fs::OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open("/home/Timbits/.config/ddrpc/daemon-log.txt")
+        .unwrap();
+    let time = Local::now();
+
+    let mut data = data.to_owned();
+    data.retain(|char| char != '\n');
+
+    if let Err(e) = writeln!(file, "[Daemon] {time} > {data}") {
+        panic!("Error: {}", e);
+    }
+}
+
+fn client_log(data: String) {
+    let mut file = std::fs::OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open("/home/Timbits/.config/ddrpc/daemon-log.txt")
+        .unwrap();
+    let time = Local::now();
+
+    let mut data = data.to_owned();
+    data.retain(|char| char != '\n');
+
+    if let Err(e) = writeln!(file, "[Client] {time} > {data}") {
+        panic!("Error: {}", e);
+    }
+}
