226a3bc74468486522ed55372ea4cb0ca85b4c61
T1mbits <timbits1123@gmail.com>
2024-04-18 23:39:44 -0400

    moved discord thread, editing conf doesn't need IPC

---
+++ src/daemon/discord.rs
@@ -0,0 +1,86 @@
+use crate::{config::structure::DiscordConfig, daemon::socket::*, discord::*, logging::ddrpc_log};
+
+use discord_rpc_client::Client;
+use std::{
+    io::{self, ErrorKind},
+    process,
+    sync::mpsc::*,
+    thread, time,
+};
+
+pub enum DiscordThreadCommands {
+    Connect,
+    Disconnect,
+    // Get,
+    Update,
+}
+
+pub fn discord_thread(
+    config: DiscordConfig,
+    sender_main: Sender<Vec<u8>>,
+) -> Result<Sender<DiscordThreadCommands>, io::Error> {
+    let (sender_discord, receiver_discord) = channel();
+
+    match thread::Builder::new()
+        .name("discord_rpc".to_owned())
+        .spawn(move || {
+            let mut config: DiscordConfig = config;
+            let sender_main: Sender<Vec<u8>> = sender_main;
+            let mut client: Option<Client> = None;
+            loop {
+                let discord_channel: DiscordThreadCommands =
+                    match receiver_discord.recv_timeout(time::Duration::from_secs(5)) {
+                        Err(error) if error == RecvTimeoutError::Timeout => {
+                            continue;
+                        }
+                        Err(error) => {
+                            ddrpc_log(&format!("All senders disconnected: {error}"));
+                            ddrpc_log("Closing Discord RPC thread");
+                            break;
+                        }
+                        Ok(str) => str,
+                    };
+                match discord_channel {
+                    DiscordThreadCommands::Connect => {
+                        if let Some(c) = &mut client {
+                            start(&config, c);
+                        } else {
+                            client = match init(&config) {
+                                Ok(mut client) => {
+                                    start(&config, &mut client);
+                                    Some(client)
+                                }
+                                Err(error) => panic!("{error}"),
+                            };
+                        }
+                    }
+                    DiscordThreadCommands::Disconnect => {
+                        if let Some(c) = &mut client {
+                            ddrpc_log("some");
+                            stop(c).unwrap();
+                        }
+                        ()
+                    }
+                    // DiscordThreadCommands::Get => match sender_main.send(get(&config)) {
+                    //     Err(error) => {
+                    //         ddrpc_log(&format!("Main thread's receiver crashed: {error}"));
+                    //         ddrpc_log("Closing Discord RPC thread");
+                    //         break;
+                    //     }
+                    //     Ok(_) => (),
+                    // },
+                    DiscordThreadCommands::Update => {
+                        if let Some(c) = &mut client {
+                            update(&mut config, c);
+                        }
+                        ddrpc_log(&format!("{:?}", config))
+                    }
+                }
+            }
+        }) {
+        Err(error) => return Err(error),
+        Ok(_) => (),
+    }
+
+    Ok(sender_discord)
+}
--- src/discord.rs
+++ src/discord.rs
@@ -1,75 +1,118 @@
-use std::{
-    io,
-    sync::mpsc::{channel, RecvTimeoutError, Sender},
-    thread, time,
+use std::io;
+
+use discord_rpc_client::{models::ActivityAssets, Client};
+
+use crate::{
+    config::{
+        read_config_file,
+        structure::{DConfig, DiscordConfig},
+        write_config,
+    },
+    logging::ddrpc_log,
+    parser::structure::CliDiscordSet,
 };
 
-use crate::{config::structure::DiscordConfig, logging::ddrpc_log};
-
-pub enum DiscordThreadCommands {
-    Connect,
-    Disconnect,
-    Get,
-    Set,
-}
-
-pub fn discord_thread(
-    config: DiscordConfig,
-    sender_main: Sender<Vec<u8>>,
-) -> Result<Sender<DiscordThreadCommands>, io::Error> {
-    let (sender_discord, receiver_discord) = channel();
-
-    match thread::Builder::new()
-        .name("discord_rpc".to_owned())
-        .spawn(move || {
-            let config: DiscordConfig = config;
-            let sender_main: Sender<Vec<u8>> = sender_main;
-            loop {
-                let discord_channel: DiscordThreadCommands =
-                    match receiver_discord.recv_timeout(time::Duration::from_secs(5)) {
-                        Err(error) if error == RecvTimeoutError::Timeout => {
-                            continue;
-                        }
-                        Err(error) => {
-                            ddrpc_log(&format!("All senders disconnected: {error}"));
-                            ddrpc_log("Closing Discord RPC thread");
-                            break;
-                        }
-                        Ok(str) => str,
-                    };
-                match discord_channel {
-                    DiscordThreadCommands::Connect => todo!(),
-                    DiscordThreadCommands::Disconnect => todo!(),
-                    DiscordThreadCommands::Get => match sender_main.send(get_command(&config)) {
-                        Err(error) => {
-                            ddrpc_log(&format!("Main thread's receiver crashed: {error}"));
-                            ddrpc_log("Closing Discord RPC thread");
-                            break;
-                        }
-                        Ok(_) => (),
-                    },
-                    DiscordThreadCommands::Set => todo!(),
-                }
-            }
-        }) {
-        Err(error) => return Err(error),
-        Ok(_) => (),
-    }
-
-    Ok(sender_discord)
-}
-
-fn get_command(config: &DiscordConfig) -> Vec<u8> {
-    format!(
-        "Client ID: {}\nDetails: {}\nLarge Image Key: {}\nLarge Image Text: {}\nSmall Image Key: {}\nSmall Image Text: {}\nState: {}",
+pub fn get(config: &DiscordConfig) {
+    println!(
+        "Client ID: {}\nDetails: {}\nState: {}\nLarge Image Key: {}\nLarge Image Text: {}\nSmall Image Key: {}\nSmall Image Text: {}",
         config.client_id,
-        {if config.details == "" {"<None>"} else {config.details.as_str()}},
-        {if config.large_image_key == "" {"<None>"} else {config.large_image_key.as_str()}},
-        {if config.large_image_text == "" {"<None>"} else {config.large_image_text.as_str()}},
-        {if config.small_image_key == "" {"<None>"} else {config.small_image_key.as_str()}},
-        {if config.small_image_text == "" {"<None>"} else {config.small_image_text.as_str()}},
-        {if config.state == "" {"<None>"} else {config.state.as_str()}}
-    )
-    .as_bytes()
-    .to_owned()
+        {if config.details.is_empty() {"<None>"} else {config.details.as_str()}},
+        {if config.state.is_empty() {"<None>"} else {config.state.as_str()}},
+        {if config.assets.large_image.is_empty() {"<None>"} else {config.assets.large_image.as_str()}},
+        {if config.assets.large_text.is_empty() {"<None>"} else {config.assets.large_text.as_str()}},
+        {if config.assets.small_image.is_empty() {"<None>"} else {config.assets.small_image.as_str()}},
+        {if config.assets.small_text.is_empty() {"<None>"} else {config.assets.small_text.as_str()}},
+    );
+}
+
+pub fn set(mut config: DConfig, arg: CliDiscordSet) {
+    if let Some(id) = arg.client_id {
+        config.discord.client_id = id
+    }
+    if let Some(details) = arg.details {
+        config.discord.details = details
+    }
+    if let Some(lik) = arg.large_image {
+        config.discord.assets.large_image = lik
+    }
+    if let Some(lit) = arg.large_text {
+        config.discord.assets.large_text = lit
+    }
+    if let Some(sik) = arg.small_image {
+        config.discord.assets.small_image = sik
+    }
+    if let Some(sit) = arg.small_text {
+        config.discord.assets.small_text = sit
+    }
+    if let Some(state) = arg.state {
+        config.discord.state = state
+    }
+    write_config(&config);
+}
+
+pub fn init(config: &DiscordConfig) -> Result<Client, io::Error> {
+    let mut client: Client = Client::new(config.client_id);
+    ddrpc_log("Attempting to start client");
+    client.start();
+    client.on_ready(|_context| {
+        ddrpc_log("Discord Client ready!");
+    });
+    client.on_error(|context| ddrpc_log(&format!("Error: {:?}", context)));
+    // match client.set_activity(|activity| activity.state("test")) {
+    //     Ok(_) => {}
+    //     Err(error) => ddrpc_log(&format!("Error while setting activity: {}", error)),
+    // }
+    // ddrpc_log("Client should have finished now");
+    return Ok(client);
+}
+
+pub fn start(config: &DiscordConfig, client: &mut Client) {
+    ddrpc_log(&format!("{:?}", config));
+    client
+        .set_activity(|activity| {
+            let mut activity = activity;
+
+            if !config.details.is_empty() {
+                activity.details = Some(config.details.to_owned());
+            }
+
+            if !config.state.is_empty() {
+                activity.state = Some(config.state.to_owned());
+            }
+
+            if !config.assets.is_empty() {
+                let mut assets = ActivityAssets::new();
+
+                if !config.assets.large_image.is_empty() {
+                    assets.large_image = Some(config.assets.large_image.to_owned());
+                }
+
+                if !config.assets.large_text.is_empty() {
+                    assets.large_text = Some(config.assets.large_text.to_owned());
+                }
+
+                if !config.assets.small_image.is_empty() {
+                    assets.small_image = Some(config.assets.small_image.to_owned());
+                }
+
+                if !config.assets.small_text.is_empty() {
+                    assets.small_text = Some(config.assets.small_text.to_owned());
+                }
+                activity.assets = Some(assets);
+            }
+            ddrpc_log(&format!("{:?}", activity));
+            activity
+        })
+        .unwrap();
+}
+
+pub fn stop(client: &mut Client) -> Result<bool, io::Error> {
+    client.clear_activity().unwrap();
+    Ok(true)
+}
+
+pub fn update(config: &mut DiscordConfig, client: &mut Client) -> () {
+    *config = read_config_file().discord;
+    ddrpc_log(&format!("{:?}", config));
+    start(&config, client);
 }
