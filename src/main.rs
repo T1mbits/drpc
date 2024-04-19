95d62f062a362e6bb8c2836067e7068965019910
T1mbits <timbits1123@gmail.com>
2024-04-18 23:39:53 -0400

    cleaned up main

--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,10 @@ pub mod discord;
 pub mod logging;
 pub mod parser;
 
-// use discord_rpc_client::Client;
-
-// use config::{initialize_config, structure::DConfig, write_config};
+use config::{initialize_config, structure::DConfig};
 use parser::cli::parse_command;
 
 fn main() {
-    // let config: DConfig = initialize_config();
-    // let mut client: Client = Client::new(config.discord.client_id);
-    // client.start();
-    // client.on_ready(|_context| {
-    //     println!("Discord Client ready!");
-    // });
-    // client.on_error(|context| println!("Error: {:?}", context));
-    // match client.set_activity(|activity| activity.state("test")) {
-    //     Ok(_) => {}
-    //     Err(error) => {
-    //         println!("Error while setting activity: {}", error)
-    //     }
-    // }
-    // ();
-    // println!("should've set activity now");
-    // write_config(&config);
-    parse_command();
+    let config: DConfig = initialize_config();
+    parse_command(config);
 }
