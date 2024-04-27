da5dce53f5ede4c00d080a6b55bd4dcdd0a451cf
T1mbits <timbits1123@gmail.com>
2024-04-26 21:54:13 -0400

    moved logging to main and removed async deps

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,50 @@
 pub mod config;
-pub mod daemon;
 pub mod discord;
-pub mod logging;
 pub mod parser;
 
-use config::{initialize_config, DConfig};
+use config::{
+    // dir_path,
+    initialize_config,
+    DConfig,
+};
 use parser::cli::parse_command;
+use tracing::{debug, Level};
+// use tracing_appender::rolling;
+use tracing_subscriber::fmt;
 
 fn main() {
     let config: DConfig = initialize_config();
     parse_command(config);
+    loop {
+        std::thread::sleep(std::time::Duration::from_secs(10));
+    }
+}
+
+pub fn log_setup() -> () {
+    // let log_files = rolling::never(dir_path() + "logs/", "ddrpc.log");
+
+    fmt()
+        // .with_writer(log_files)
+        // .with_ansi(false)
+        .with_target(false)
+        .with_max_level(Level::INFO)
+        .init();
+}
+
+pub fn log_setup_debug() -> () {
+    // let log_files = rolling::never(dir_path() + "logs/", "ddrpc_debug.log");
+
+    // let filter = EnvFilter::try_from_default_env()
+    //     .or_else(|_| EnvFilter::try_new("warn"))
+    //     .unwrap()
+    //     .add_directive("ddrpc=trace".parse().unwrap());
+
+    fmt()
+        // .with_writer(log_files)
+        // .with_ansi(false)
+        // .with_env_filter(filter)
+        .with_max_level(Level::TRACE)
+        .init();
+
+    debug!("Logger setup");
 }
