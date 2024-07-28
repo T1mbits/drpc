0f9ec5de449a5e3bb160be66abafba27f7c0c6bc
Timbits <timbits1123@gmail.com>
2024-07-27 22:23:41 -0400

    Added config

---
+++ common/src/config/mod.rs
@@ -0,0 +1,88 @@
+mod types;
+
+use anyhow::Context;
+use dirs::config_dir;
+use std::{fs, path::PathBuf};
+use toml::{from_str, to_string};
+pub use types::*;
+
+fn config_path() -> PathBuf {
+    match config_dir() {
+        Some(dir) => PathBuf::from(format!("{}/ddrpc", dir.display())),
+        None => PathBuf::from("./ddrpc"),
+    }
+}
+
+fn config_file() -> PathBuf {
+    config_path().join("ddrpc.toml")
+}
+
+pub fn write_config(config: &Config) -> anyhow::Result<()> {
+    let config_dir = config_path();
+    let serialized_config = to_string(config).context("could not serialize config")?;
+
+    if !config_dir.exists() {
+        fs::create_dir_all(config_dir)?;
+    }
+
+    Ok(fs::write(config_file(), serialized_config)?)
+}
+
+pub fn get_config(overwrite: bool) -> anyhow::Result<Config> {
+    let config_path = config_file();
+
+    match from_str(
+        &String::from_utf8(fs::read(&config_path).context(format!(
+            "failed to read config from {}",
+            config_path.display()
+        ))?)
+        .context(format!(
+            "failed to deserialize config file at {}",
+            config_path.display()
+        ))?,
+    ) {
+        Ok(mut config) => {
+            empty_config(&mut config);
+            Ok(config)
+        }
+        Err(err) => {
+            if overwrite {
+                let config = Config::default();
+                write_config(&config).context(format!(
+                    "failed to overwrite config at {}",
+                    config_path.display()
+                ))?;
+                return Ok(config);
+            };
+
+            Err(err.into())
+        }
+    }
+}
+
+/// Convert all empty values within the config to None.
+pub fn empty_config(config: &mut Config) {
+    empty_template(&mut config.activity)
+}
+
+/// Convert all empty strings and templates to None.
+pub fn empty_template(activity: &mut ActivityTemplate) -> () {
+    fn empty_string_to_none(value: &mut Option<String>) {
+        if value.as_ref().map_or(false, |s| s.is_empty()) {
+            *value = None;
+        }
+    }
+
+    activity.foreach_field(empty_string_to_none);
+
+    if let Some(assets) = &activity.assets {
+        if assets.is_empty() {
+            activity.assets = None;
+        }
+    }
+    if let Some(buttons) = &activity.buttons {
+        if buttons.is_empty() {
+            activity.buttons = None;
+        }
+    }
+}
---
+++ common/src/config/types.rs
@@ -0,0 +1,265 @@
+use discord_rich_presence::activity::{Activity, Assets, Button};
+use serde::{Deserialize, Serialize};
+use std::fmt::Display;
+
+pub trait Template<'a, A> {
+    /// Convert the template into the actual struct.
+    fn evaluate(&'a self) -> A;
+    /// Run a function over every `Option<String>` field in the struct. Should also call foreach_field on any other fields implementing `Template`.
+    fn foreach_field<F>(&mut self, func: F)
+    where
+        F: Fn(&mut Option<String>);
+    /// Whether or not every Option field in the struct is None
+    fn is_empty(&self) -> bool;
+}
+
+/// Display an Option\<T> as either its display value or \<None>.
+fn as_string<T>(value: &Option<T>) -> String
+where
+    T: Display,
+{
+    match value {
+        Some(val) => val.to_string(),
+        None => "<None>".to_string(),
+    }
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq)]
+pub struct Config {
+    pub activity: ActivityTemplate,
+    // pub processes: ProcessesConfig,
+    // pub spotify: SpotifyConfig,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            activity: ActivityTemplate::default(),
+            // processes: ProcessesConfig {
+            //     idle_image: "idle".to_string(),
+            //     idle_text: "Idle".to_string(),
+            //     processes: vec![ProcessConfig {
+            //         image: "code".to_string(),
+            //         name: "code".to_string(),
+            //         text: "Visual Studio Code".to_string(),
+            //     }],
+            // },
+            // spotify: SpotifyConfig {
+            //     client_id: String::new(),
+            //     client_secret: String::new(),
+            //     refresh_token: String::new(),
+            // },
+        }
+    }
+}
+
+/// A struct closely mirroring [`discord_rich_presence::activity::Activity`], but can be both serialized and deserialized for storage in a config file.
+///
+/// May also have text templates such as {{process.name}} that needs to be evaluated before being converted to [`discord_rich_presence::activity::Activity`]
+#[derive(Clone, Debug, Default, Serialize, Deserialize, PartialEq)]
+pub struct ActivityTemplate {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub assets: Option<AssetsTemplate>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub buttons: Option<ButtonsTemplate>,
+    // TODO add timestamps
+    // pub timestamps: TimestampsTemplate,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub details: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub state: Option<String>,
+}
+
+impl<'a> Template<'a, Activity<'a>> for ActivityTemplate {
+    fn evaluate(&self) -> Activity {
+        let mut activity = Activity::new();
+
+        if let Some(assets) = &self.assets {
+            activity = activity.assets(assets.evaluate());
+        }
+        if let Some(buttons) = &self.buttons {
+            activity = activity.buttons(buttons.evaluate());
+        }
+        if let Some(details) = &self.details {
+            activity = activity.details(details);
+        }
+        if let Some(state) = &self.state {
+            activity = activity.state(state);
+        }
+
+        activity
+    }
+
+    fn foreach_field<F>(&mut self, func: F)
+    where
+        F: Fn(&mut Option<String>),
+    {
+        if let Some(assets) = self.assets.as_mut() {
+            assets.foreach_field(&func);
+        }
+        if let Some(buttons) = self.buttons.as_mut() {
+            buttons.foreach_field(&func);
+        }
+        func(&mut self.details);
+        func(&mut self.state);
+    }
+
+    fn is_empty(&self) -> bool {
+        self.assets.is_none()
+            && self.buttons.is_none()
+            && self.details.is_none()
+            && self.state.is_none()
+    }
+}
+
+impl Display for ActivityTemplate {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "details: {}\nstate: {}\nassets: \n\t{}\nbuttons: \n\t{}",
+            as_string(&self.details),
+            as_string(&self.state),
+            as_string(&self.assets),
+            as_string(&self.buttons),
+        )
+    }
+}
+
+#[derive(Clone, Debug, Default, Serialize, Deserialize, PartialEq)]
+pub struct AssetsTemplate {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub large_image: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub large_text: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub small_image: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub small_text: Option<String>,
+}
+
+impl<'a> Template<'a, Assets<'a>> for AssetsTemplate {
+    fn evaluate(&self) -> Assets {
+        let mut assets = Assets::new();
+
+        if let Some(large_image) = &self.large_image {
+            assets = assets.large_image(large_image);
+        }
+        if let Some(large_text) = &self.large_text {
+            assets = assets.large_text(large_text);
+        }
+        if let Some(small_image) = &self.small_image {
+            assets = assets.small_image(small_image);
+        }
+        if let Some(small_text) = &self.small_text {
+            assets = assets.small_text(small_text);
+        }
+
+        assets
+    }
+
+    fn foreach_field<F>(&mut self, func: F)
+    where
+        F: Fn(&mut Option<String>),
+    {
+        func(&mut self.large_image);
+        func(&mut self.large_text);
+        func(&mut self.small_image);
+        func(&mut self.small_text);
+    }
+
+    fn is_empty(&self) -> bool {
+        self.large_image.is_none()
+            && self.large_text.is_none()
+            && self.small_image.is_none()
+            && self.small_text.is_none()
+    }
+}
+
+impl Display for AssetsTemplate {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "large image: {}\n\tlarge image text: {}\n\tsmall image: {}\n\tsmall image text: {}",
+            as_string(&self.large_image),
+            as_string(&self.large_text),
+            as_string(&self.small_image),
+            as_string(&self.small_text),
+        )
+    }
+}
+
+#[derive(Clone, Debug, Default, Serialize, Deserialize, PartialEq)]
+pub struct ButtonsTemplate {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub label1: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub url1: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub label2: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub url2: Option<String>,
+}
+
+impl<'b> Template<'b, Vec<Button<'b>>> for ButtonsTemplate {
+    fn evaluate(&self) -> Vec<Button> {
+        let mut buttons: Vec<Button> = Vec::new();
+
+        if let (Some(label), Some(url)) = (&self.label1, &self.url1) {
+            buttons.push(Button::new(label, url));
+        }
+        if let (Some(label), Some(url)) = (&self.label2, &self.url2) {
+            buttons.push(Button::new(label, url));
+        }
+
+        buttons
+    }
+    fn foreach_field<F>(&mut self, func: F)
+    where
+        F: Fn(&mut Option<String>),
+    {
+        func(&mut self.label1);
+        func(&mut self.url1);
+        func(&mut self.label2);
+        func(&mut self.url2);
+    }
+
+    fn is_empty(&self) -> bool {
+        self.label1.is_none() && self.url1.is_none() && self.label2.is_none() && self.url2.is_none()
+    }
+}
+
+impl Display for ButtonsTemplate {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "button 1 label: {}\n\tbutton 1 url: {}\n\tbutton 2 label: {}\n\tbutton 2 url: {}",
+            as_string(&self.label1),
+            as_string(&self.url1),
+            as_string(&self.label2),
+            as_string(&self.url2),
+        )
+    }
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq)]
+pub struct ProcessesConfig {
+    pub idle_image: String,
+    pub idle_text: String,
+    #[serde(default, skip_serializing_if = "Vec::is_empty")]
+    pub processes: Vec<ProcessConfig>,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq)]
+pub struct ProcessConfig {
+    pub image: String,
+    pub name: String,
+    pub text: String,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq)]
+pub struct SpotifyConfig {
+    pub client_id: String,
+    pub client_secret: String,
+    // pub fallback: SpotifyFallbackConfig,
+    pub refresh_token: String,
+}
