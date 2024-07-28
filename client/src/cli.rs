b4df659a5f19934732af183e0de6bfbcc69210f1
Timbits <timbits1123@gmail.com>
2024-07-27 22:25:56 -0400

    Overhauled CLI Completely changed CLI layout

--- client/src/cli.rs
+++ client/src/cli.rs
@@ -2,175 +2,162 @@ use clap::{Args, Parser, Subcommand};
 
 #[derive(Debug, Parser)]
 #[command(version, about = "Dynamic Discord Rich Presence Customizer")]
-pub struct Ddrpc {
-    #[arg(short = 'd', long, help = "Enable debug logs")]
-    pub debug: bool,
+pub struct Cli {
     #[arg(
         short = 'o',
         long,
         help = "Overwrite an invalid config with a default config"
     )]
     pub config_overwrite: bool,
+    #[arg(short = 'd', long, help = "Enable debug logs")]
+    pub debug: bool,
     #[command(subcommand)]
-    pub subcommands: DdrpcSubcommands,
+    pub subcommand: Ddrpc,
     #[arg(short = 'v', long, help = "Enable verbose output")]
     pub verbose: bool,
 }
 
 #[derive(Debug, Subcommand)]
-pub enum DdrpcSubcommands {
-    #[command(about = "Manage Discord activity data and connection")]
-    Discord(DdrpcDiscord),
-    #[command(about = "Kills the active daemon")]
+pub enum Ddrpc {
+    #[command(about = "Connect the Discord rich presence with the given application id")]
+    Connect {
+        #[arg(help = "The application id of the Discord rich presence client")]
+        id: u64,
+    },
+    #[command(about = "Disconnect the Discord rich presence")]
+    Disconnect,
+    #[command(about = "Get information from the config file or daemon")]
+    Get {
+        // TODO add command/flag to get information from the daemon
+        #[command(subcommand)]
+        subcommand: DdrpcGet,
+    },
+    #[command(about = "Kills the daemon")]
     Kill,
-    #[command(about = "Manipulate target processes")]
-    Processes(DdrpcProcesses),
-    #[command(about = "Manage your Spotify account and app connection\ntodo")]
-    Spotify(DdrpcSpotify),
-}
-
-#[derive(Debug, Args)]
-pub struct DdrpcDiscord {
-    #[command(subcommand)]
-    pub subcommands: DdrpcDiscordSubcommands,
+    #[command(about = "Edit configuration data")]
+    Set {
+        #[command(subcommand)]
+        subcommand: DdrpcSet,
+    },
+    #[command(
+        about = "Sync the daemon configuration with the local configuration. No flags syncs all configs"
+    )]
+    Sync(DdrpcSync),
 }
 
 #[derive(Debug, Subcommand)]
-pub enum DdrpcDiscordSubcommands {
-    #[command(about = "Start and connect Discord IPC client and set activity")]
-    Connect(DdrpcDiscordConnect),
-    #[command(about = "Clear the Discord activity\ntodo")]
-    Disconnect,
-    #[command(about = "Get Discord activity data")]
-    Get(DdrpcDiscordGet),
-    #[command(about = "Set Discord activity data")]
-    Set(DdrpcDiscordSet),
-    #[command(
-        about = "Update Discord activity data (sync app and config file). No longer useful\ntodo"
-    )]
-    Update,
+pub enum DdrpcGet {
+    #[command(about = "Show Discord activity configuration")]
+    Activity,
+    #[command(about = "Get target processes list or all actively running processes")]
+    Processes {
+        #[command(subcommand)]
+        subcommand: DdrpcGetProcesses,
+    },
+    #[command(about = "Show Spotify client configuration")]
+    Spotify,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum DdrpcGetProcesses {
+    #[command(about = "Get all actively running processes")]
+    All,
+    #[command(about = "Show targeted processes configuration")]
+    Target,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum DdrpcSet {
+    #[command(about = "Set activity fields for the Discord rich presence")]
+    Activity(DdrpcSetActivity),
+    #[command(about = "Set the list of detectable processes and their order")]
+    Processes {
+        #[command(subcommand)]
+        subcommand: DdrpcSetProcesses,
+    },
+    #[command(about = "Set the Spotify client id and secret")]
+    #[group(required = true)]
+    Spotify {
+        #[arg(long, group = "test")]
+        client_id: Option<String>,
+        #[arg(long, group = "test")]
+        client_secret: Option<String>,
+    },
 }
 
 #[derive(Debug, Args)]
-pub struct DdrpcDiscordConnect {
-    #[arg(help = "The application id of your Discord presence application")]
-    pub id: u64,
-}
-
-#[derive(Debug, Args)]
-pub struct DdrpcDiscordGet {
-    #[arg(short = 'd', long, help = "todo")]
-    pub daemon: bool,
-}
-
-#[derive(Debug, Args)]
-pub struct DdrpcDiscordSet {
-    #[arg(short = 'c', long, help = "Set the Discord application id")]
-    pub client_id: Option<u64>,
-    #[arg(short = 'd', long, help = "Set the activity details")]
+#[group(multiple = true, required = true)]
+pub struct DdrpcSetActivity {
+    #[arg(short = 'd', long, help = "Set activity details")]
     pub details: Option<String>,
-    #[arg(short = 's', long, help = "Set the activity state")]
+    #[arg(short = 's', long, help = "Set activity state")]
     pub state: Option<String>,
-    #[arg(short = 'I', long, help = "Set the activity large image")]
+    #[arg(short = 'I', long, help = "Set large image")]
     pub large_image: Option<String>,
-    #[arg(short = 'i', long, help = "Set the activity large image text")]
+    #[arg(short = 'i', long, help = "Set large image text")]
     pub small_image: Option<String>,
-    #[arg(short = 'T', long, help = "Set the activity small image")]
+    #[arg(short = 'T', long, help = "Set small image")]
     pub large_text: Option<String>,
-    #[arg(short = 't', long, help = "Set the activity small image text")]
+    #[arg(short = 't', long, help = "Set small image text")]
     pub small_text: Option<String>,
-    #[arg(short = 'b', long, help = "Set the activity button 1 text")]
-    pub button1_text: Option<String>,
-    #[arg(short = 'u', long, help = "Set the activity button 1 url")]
+    #[arg(short = 'b', long, help = "Set button 1 text")]
+    pub button1_label: Option<String>,
+    #[arg(short = 'u', long, help = "Set button 1 url")]
     pub button1_url: Option<String>,
-    #[arg(short = 'B', long, help = "Set the activity button 2 text")]
-    pub button2_text: Option<String>,
-    #[arg(short = 'U', long, help = "Set the activity button 2 url")]
+    #[arg(short = 'B', long, help = "Set button 2 text")]
+    pub button2_label: Option<String>,
+    #[arg(short = 'U', long, help = "Set button 2 url")]
     pub button2_url: Option<String>,
 }
 
-#[derive(Debug, Args)]
-pub struct DdrpcProcesses {
-    #[command(subcommand)]
-    pub subcommands: DdrpcProcessesSubcommands,
-}
-
 #[derive(Debug, Subcommand)]
-pub enum DdrpcProcessesSubcommands {
-    #[command(about = "Add a process")]
-    Add(DdrpcProcessesAdd),
-    #[command(about = "List all target processes")]
-    List,
-    #[command(about = "Reorder target process priorities")]
-    Priority(DdrpcProcessesPriority),
-    #[command(about = "Remove a process")]
-    Remove(DdrpcProcessesRemove),
-    #[command(about = "Show all active processes (may not include)")]
-    Show,
-}
-
-#[derive(Debug, Args, Clone)]
-pub struct DdrpcProcessesAdd {
-    #[arg(
-        index = 3,
-        help = "Set the image URL or Discord asset name for the process"
-    )]
-    pub image: String,
-    #[arg(index = 1, help = "Name of the process being added")]
-    pub name: String,
-    #[arg(index = 2, help = "Set the text associated with this process")]
-    pub text: String,
-}
-
-#[derive(Debug, Args)]
-pub struct DdrpcProcessesPriority {
-    #[arg(help = "Name of the process entry operated on")]
-    pub name: String,
-    #[command(flatten)]
-    pub operation: DdrpcProcessesPriorityOperation,
+pub enum DdrpcSetProcesses {
+    #[command(about = "Add a process to be detected")]
+    Add {
+        #[arg(index = 3, help = "Set the image URL or Discord asset name")]
+        image: String,
+        #[arg(index = 1, help = "Set the name of the processes being added")]
+        name: String,
+        #[arg(index = 2, help = "Set the text to be displayed for this processes")]
+        text: String,
+    },
+    #[command(about = "Change the order of which processes are detected first")]
+    Order {
+        #[arg()]
+        name: String,
+        #[command(flatten)]
+        test: DdrpcSetProcessesOrder,
+    },
+    #[command(about = "Remove a target process")]
+    Remove {
+        #[arg(help = "Name of the processes being removed")]
+        name: String,
+    },
 }
 
 #[derive(Debug, Args)]
-#[group(multiple = false, required = true)]
-pub struct DdrpcProcessesPriorityOperation {
-    #[arg(short = 'd', long, help = "Lowers process priority by 1")]
+#[group(required = true)]
+pub struct DdrpcSetProcessesOrder {
+    #[arg(short = 'd', long, help = "Decrease the order of the process")]
     pub decrease: bool,
-    #[arg(short = 'i', long, help = "Increases process priority by 1")]
+    #[arg(short = 'i', long, help = "Increase the order of the process")]
     pub increase: bool,
-    #[arg(
-        short = 's',
-        long,
-        help = "Set process priority manually. Highest priority is 0"
-    )]
-    pub set: Option<usize>,
+    #[arg(short = 's', long, help = "Set the order of the process")]
+    pub set: Option<u32>,
 }
 
 #[derive(Debug, Args)]
-pub struct DdrpcProcessesRemove {
-    #[arg(help = "Name of the process being removed")]
-    pub name: String,
+pub struct DdrpcSync {
+    #[arg(short = 'a', long, help = "Sync the activity configuration")]
+    pub activity: bool,
+    #[arg(short = 'p', long, help = "Sync the processes configuration")]
+    pub processes: bool,
+    #[arg(short = 's', long, help = "Sync the Spotify configuration")]
+    pub spotify: bool,
 }
 
-#[derive(Debug, Args)]
-pub struct DdrpcSpotify {
-    #[command(subcommand)]
-    pub subcommands: DdrpcSpotifySubcommands,
-}
-
-#[derive(Debug, Subcommand)]
-pub enum DdrpcSpotifySubcommands {
-    #[command(about = "Link your spotify account???")]
-    Add,
-    #[command(about = "Set Spotify application data")]
-    Client(DdrpcSpotifyClient),
-    #[command(about = "Unlink your spotify account???")]
-    Remove,
-}
-
-#[derive(Debug, Args)]
-pub struct DdrpcSpotifyClient {
-    #[arg(help = "Set Spotify client ID")]
-    pub id: Option<String>,
-    #[arg(help = "Set Spotify client secret")]
-    pub secret: Option<String>,
+impl DdrpcSync {
+    pub fn no_flags(&self) -> bool {
+        !(self.activity || self.processes || self.spotify)
+    }
 }
